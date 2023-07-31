0f7e254f4d1753e083a8e051d52f75e23c0e10d7
Mohammed Almarzooq <md_almarzooq@hotmail.com>
2023-07-31 13:22:07 +0300

    created users router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const usersCtrl = require('../controllers/users');
+
+// GET /users   => get	/		index
+router.get('/', usersCtrl.index);
+
+// GET /users/:id/edit =>  get 	/:id/edit		edit
+router.get('/:id/edit', usersCtrl.edit);
+
+// PUT /users/:id/ =>  put	/:id		update
+router.put('/:id', usersCtrl.update);
+
+// DELETE /users/:id =>  Delete	/:id		destroy
+router.delete('/:id', usersCtrl.destroy);
 
 module.exports = router;
