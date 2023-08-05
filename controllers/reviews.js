3eb905bb9b466e5d80c7a6c2348643caa547f6c7
ahmedmohamed2302 <ahmedmohamed20182302@gmail.com>
2023-08-05 23:11:33 +0300

    edit the validation for reviews controller - needs test

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,29 +4,30 @@ const User = require('../models/user');
 const { checkSchema, validationResult } = require('express-validator');
 
 
-const reviewFormCheckSchema = {
+const reviewRouteParametersCheckSchema = {
 
-  id: {
-    in: ['params'],
-    optional: true,
-    isMongoId: {
-      errorMessage: 'Invalid ID'
-    }
-  },
-  res_id: {
-    in: ['params'],
-    optional: true,
-    isMongoId: {
-      errorMessage: 'Invalid ID'
-    }
-  },
-  item_id: {
-    in: ['params'],
-    optional: true,
-    isMongoId: {
-      errorMessage: 'Invalid ID'
-    }
-  },
+    id: {
+      in: ['params'],
+      optional: true,
+      isMongoId: {
+        errorMessage: 'Invalid ID'
+      }
+    },
+    res_id: {
+      in: ['params'],
+      isMongoId: {
+        errorMessage: 'Invalid ID'
+      }
+    },
+    item_id: {
+      in: ['params'],
+      isMongoId: {
+        errorMessage: 'Invalid ID'
+      }
+    },
+}
+
+const reviewFormCheckSchema = {
 
   user: {
     in: ['body'],
@@ -53,6 +54,7 @@ const reviewFormCheckSchema = {
     optional: true,
     custom: {
       options: (value, { req }) => {
+        value = value.trim();
         if (value) {
           if(value.length < 3 || value.length >255){
             return false
@@ -88,6 +90,22 @@ async function create(req, res) {
 
   // validation
   try {
+    // Manually run checkSchema to validate the route Parameters
+    await checkSchema(reviewRouteParametersCheckSchema).run(req);
+  
+
+    // Check if there are any validation errors
+    const routeParametersErrors = validationResult(req);
+
+    if (!routeParametersErrors.isEmpty()) {
+        const errors_mapped = routeParametersErrors.mapped(); 
+        // check if one of the params IDs return an error
+        if(errors_mapped.res_id || errors_mapped.item_id){
+          req.flash('error', 'dont play with me please');
+          return res.redirect(301, `/`);
+        }
+    }
+
     // Manually run checkSchema to validate the request body
     await checkSchema(reviewFormCheckSchema).run(req);
 
@@ -95,13 +113,6 @@ async function create(req, res) {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-
-        // check if one of the params IDs return an error
-        const errors_mapped = errors.mapped();
-        if(errors_mapped.id || errors_mapped.res_id || errors_mapped.item_id){
-          req.flash('error', 'dont play with me please');
-          return res.redirect(301, `/`);
-        }
         req.session.formBody = req.body;
         req.session.errors = errors.mapped();
         req.flash('error', 'validation errors');
@@ -118,6 +129,10 @@ async function create(req, res) {
    // Remove empty properties so that defaults will be applied
    for (let key in req.body) {
      if (req.body[key] === '') delete req.body[key];
+   }
+
+   if(req.body.comment){
+      req.body.comment = req.body.comment.trim();
    }
  
    try {
@@ -157,9 +172,25 @@ async function create(req, res) {
 
 async function update(req, res) {
 
-
     // validation
     try {
+
+      // Manually run checkSchema to validate the route Parameters
+      await checkSchema(reviewRouteParametersCheckSchema).run(req);
+    
+
+      // Check if there are any validation errors
+      const routeParametersErrors = validationResult(req);
+
+      if (!routeParametersErrors.isEmpty()) {
+          const errors_mapped = routeParametersErrors.mapped(); 
+          // check if one of the params IDs return an error
+          if(errors_mapped.res_id || errors_mapped.item_id || errors_mapped.id ){
+            req.flash('error', 'dont play with me please');
+            return res.redirect(301, `/`);
+          }
+      }
+
       // Manually run checkSchema to validate the request body
       await checkSchema(reviewFormCheckSchema).run(req);
 
@@ -190,6 +221,10 @@ async function update(req, res) {
    // Remove empty properties so that defaults will be applied
    for (let key in req.body) {
      if (req.body[key] === '') delete req.body[key];
+   }
+
+   if(req.body.comment){
+      req.body.comment = req.body.comment.trim();
    }
   
    try {
@@ -237,6 +272,21 @@ async function update(req, res) {
 
 async function destroy(req, res) {
   try {
+      // Manually run checkSchema to validate the route Parameters
+      await checkSchema(reviewRouteParametersCheckSchema).run(req);
+    
+      // Check if there are any validation errors
+      const routeParametersErrors = validationResult(req);
+
+      if (!routeParametersErrors.isEmpty()) {
+          const errors_mapped = routeParametersErrors.mapped(); 
+          // check if one of the params IDs return an error
+          if(errors_mapped.res_id || errors_mapped.item_id || errors_mapped.id ){
+            req.flash('error', 'dont play with me please');
+            return res.redirect(301, `/`);
+          }
+      }
+
       const restaurant = await Restaurant.findById(req.params.res_id);
       if(restaurant){
         const item = restaurant.menu.id(req.params.item_id);
