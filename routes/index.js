d6f112e979f93032fd8f187ea6e2ebf505185456
Mohammed Almarzooq <md_almarzooq@hotmail.com>
2023-08-08 06:13:08 +0300

    ramized, recomend only restaurants with rating > 4

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,48 @@ const restaurant = require('../models/restaurant');
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try{
-    const restaurants = await restaurant.find({});
+    const randomRestaurants = await restaurant.find({});
     const successMessages = req.flash('success');
     const errorMessages = req.flash('error');
+    let restaurants = []
+    
+    //variables to randomize array
+    let index = randomRestaurants.length;
+    let randomIndex;
+
+    // While there remain elements to shuffle.
+    while (index != 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * index);
+      index--;
+
+      // And swap it with the current element.
+      [randomRestaurants[index], randomRestaurants[randomIndex]] = [randomRestaurants[randomIndex], randomRestaurants[index]];
+    }
+
+    // get only restaurants with average rating higher than 4
+
+    for (let i=0; i<randomRestaurants.length; i++) {
+      let count=0;
+      let total=0;
+      let avr=0;
+      randomRestaurants[i].menu.forEach ((item, i) => {
+        item.reviews.forEach ((review , r) => {
+        total += review.rating;
+        count++;
+      });
+      });
+      if (total ===0) {
+        
+      }else {
+        avr= total/count
+        if (avr >= 4){
+          restaurants.push(randomRestaurants[i]);
+        }
+      }
+    }
+    
+
     res.render('index', { title: 'Home Page' , restaurants, successMessages, errorMessages});
   }catch(err){
     console.log(err);
